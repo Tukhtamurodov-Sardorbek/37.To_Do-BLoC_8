91ce61fe8d6fbccd6783e4a3e0d12927b4f59693
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 12:45:36 +0500

    🛡 Code Optimized 🛡

---
+++ lib/pages/recycle_bin.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecycleBin extends StatelessWidget {
+  const RecycleBin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
