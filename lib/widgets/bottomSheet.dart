6c26b2a9d72b4b16973032107b6a6023eec0240a
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 15:05:03 +0500

    🛡 Code Optimized 🛡

--- lib/widgets/bottomSheet.dart
+++ lib/widgets/bottomSheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/bloc/bloc_exports.dart';
 import 'package:todo_app/models/todo_model.dart';
+import 'package:todo_app/services/color_service.dart';
 import 'package:todo_app/services/guid_gen.dart';
 
 class BottomSheetContent extends StatelessWidget {
@@ -17,9 +18,12 @@ class BottomSheetContent extends StatelessWidget {
         MediaQuery.of(context).viewInsets.bottom,
       ),
       children: [
-        const Text(
+        Text(
           'Add ToDo',
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: ColorService.main),
           textAlign: TextAlign.center,
         ),
         const SizedBox(height: 10.0),
@@ -27,34 +31,58 @@ class BottomSheetContent extends StatelessWidget {
           autocorrect: true,
           autofocus: true,
           controller: titleController,
-          decoration: const InputDecoration(
-            label: Text('Title'),
-            border: OutlineInputBorder(),
+          decoration: InputDecoration(
+            label: Text(
+              'Title',
+              style: TextStyle(color: ColorService.main),
+            ),
+            focusColor: ColorService.main,
+            border: const OutlineInputBorder(),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: ColorService.main, width: 5.0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: ColorService.main, width: 3.0),
+            ),
           ),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text(
-                  'Cancel',
-                  style: TextStyle(
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                )),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  color: ColorService.main,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
             ElevatedButton(
-                onPressed: () {
-                  var todo = ToDo(
-                      id: GUIDGen.generate(),
-                      title: titleController.text.trim());
-                  context.read<ToDoBloc>().add(AddToDo(todo: todo));
-                  Navigator.pop(context);
-                },
-                child: const Text('Add')),
+              onPressed: () {
+                var todo = ToDo(
+                  id: GUIDGen.generate(),
+                  title: titleController.text.trim(),
+                );
+                context.read<ToDoBloc>().add(AddToDo(todo: todo));
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                primary: ColorService.main,
+              ),
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: ColorService.white,
+                ),
+              ),
+            ),
           ],
         )
       ],
