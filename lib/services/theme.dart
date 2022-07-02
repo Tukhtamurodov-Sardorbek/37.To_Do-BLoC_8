4439da378fecac32509fc073fc2566f563dbfa68
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 22:58:41 +0500

    🛡 Code Optimized 🛡

--- lib/services/theme.dart
+++ lib/services/theme.dart
@@ -10,69 +10,52 @@ class Themes {
   static final appThemeData = {
     AppTheme.darkTheme: ThemeData(
       brightness: Brightness.dark,
-      primarySwatch: ColorService.grey,
-      primaryColor: ColorService.black,
-      backgroundColor: ColorService.backgroundDarkTheme,
-      dividerColor: ColorService.main,
-
-      appBarTheme: AppBarTheme(
-        backgroundColor: ColorService.main,
-        foregroundColor: ColorService.white,
-      ),
-
       floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: ColorService.main,
+        backgroundColor: ColorService.lightMain2,
         foregroundColor: ColorService.white,
       ),
-
-      textButtonTheme: TextButtonThemeData(
-        style: ButtonStyle(
-          foregroundColor: MaterialStateProperty.all(ColorService.white),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        selectedItemColor: ColorService.lightMain2,
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
         ),
-      ),
-
-      textTheme: const TextTheme(
-        subtitle1: TextStyle(color: ColorService.white),
-      ),
-
-      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-        backgroundColor: ColorService.grey,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: ColorService.white,
+        unselectedLabelStyle:
+            const TextStyle(fontWeight: FontWeight.normal, fontSize: 12),
       ),
     ),
-
     AppTheme.lightTheme: ThemeData(
       brightness: Brightness.light,
       primarySwatch: ColorService.grey,
       primaryColor: ColorService.white,
       backgroundColor: ColorService.backgroundLightTheme,
-      dividerColor: const Color(0xff757575),
-
+      dividerColor: ColorService.main,
       appBarTheme: AppBarTheme(
         backgroundColor: ColorService.main,
         foregroundColor: ColorService.white,
       ),
-
       floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: ColorService.main,
         foregroundColor: ColorService.white,
       ),
-
       textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
           foregroundColor: MaterialStateProperty.all(ColorService.black),
         ),
       ),
-
       textTheme: const TextTheme(
         subtitle1: TextStyle(color: ColorService.black),
       ),
-
-      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-        backgroundColor: ColorService.grey,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: ColorService.white,
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: ColorService.main,
+        selectedItemColor: ColorService.white,
+        unselectedItemColor: ColorService.grey,
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+        ),
+        unselectedLabelStyle:
+            const TextStyle(fontWeight: FontWeight.normal, fontSize: 12),
       ),
     ),
   };
