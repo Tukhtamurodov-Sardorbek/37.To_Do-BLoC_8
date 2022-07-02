f84769ad8005d97c269defd9373eceb592e024d4
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 22:23:47 +0500

    🛡 Code Optimized 🛡

--- lib/widgets/bottomSheet.dart
+++ lib/widgets/bottomSheet.dart
@@ -10,82 +10,108 @@ class BottomSheetContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final titleController = TextEditingController();
-    return ListView(
+    final descriptionController = TextEditingController();
+    return SingleChildScrollView(
       padding: EdgeInsets.fromLTRB(
         20.0,
         20.0,
         20.0,
         MediaQuery.of(context).viewInsets.bottom,
       ),
-      children: [
-        Text(
-          'Add ToDo',
-          style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              color: ColorService.main),
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 10.0),
-        TextField(
-          autocorrect: true,
-          autofocus: true,
-          controller: titleController,
-          decoration: InputDecoration(
-            label: Text(
-              'Title',
-              style: TextStyle(color: ColorService.main),
-            ),
-            focusColor: ColorService.main,
-            border: const OutlineInputBorder(),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: ColorService.main, width: 5.0),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: ColorService.main, width: 3.0),
+      child: Column(
+        children: [
+          Text(
+            'Add ToDo',
+            style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: ColorService.main),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10.0),
+          TextField(
+            autofocus: true,
+            textInputAction: TextInputAction.next,
+            controller: titleController,
+            decoration: InputDecoration(
+              label: Text(
+                'Title',
+                style: TextStyle(color: ColorService.main),
+              ),
+              focusColor: ColorService.main,
+              border: const OutlineInputBorder(),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: ColorService.main, width: 5.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: ColorService.main, width: 3.0),
+              ),
             ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text(
-                'Cancel',
-                style: TextStyle(
-                  color: ColorService.main,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
+          const SizedBox(height: 20.0),
+          TextField(
+            maxLines: null,
+            textInputAction: TextInputAction.newline,
+            keyboardType: TextInputType.multiline,
+            controller: descriptionController,
+            decoration: InputDecoration(
+              label: Text(
+                'Description',
+                style: TextStyle(color: ColorService.main),
+              ),
+              focusColor: ColorService.main,
+              border: const OutlineInputBorder(),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: ColorService.main, width: 5.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: ColorService.main, width: 3.0),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                var todo = ToDo(
-                  id: GUIDGen.generate(),
-                  title: titleController.text.trim(),
-                );
-                context.read<ToDoBloc>().add(AddToDo(todo: todo));
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                primary: ColorService.main,
-              ),
-              child: const Text(
-                'Add',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17,
-                  color: ColorService.white,
+          ),
+          const SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(
+                    color: ColorService.main,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
                 ),
               ),
-            ),
-          ],
-        )
-      ],
+              ElevatedButton(
+                onPressed: () {
+                  var todo = ToDo(
+                    id: GUIDGen.generate(),
+                    title: titleController.text.trim(),
+                    description: descriptionController.text.trim(),
+                  );
+                  context.read<ToDoBloc>().add(AddToDo(todo: todo));
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: ColorService.main,
+                ),
+                child: const Text(
+                  'Add',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                    color: ColorService.white,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
