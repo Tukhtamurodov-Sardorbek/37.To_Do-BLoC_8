cb499351e315cf0c242f7e258851a98af961f740
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 13:11:02 +0500

    🛡 Code Optimized 🛡

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -15,22 +15,32 @@ class CustomDrawer extends StatelessWidget {
           children: [
             Container(
               color: Colors.deepPurpleAccent.shade700,
-              padding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 20.0),
-              child: Text('Todo Drawer', style: Theme.of(context).textTheme.headline5,),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 14.0, horizontal: 20.0),
+              child: Text(
+                'Todo Drawer',
+                style: Theme.of(context).textTheme.headline5,
+              ),
             ),
             BlocBuilder<ToDoBloc, ToDoState>(
               builder: (context, state) {
                 return ListTile(
-                  leading: Icon(Icons.folder_special, color: Colors.deepPurpleAccent.shade700,),
+                  leading: Icon(
+                    Icons.folder_special,
+                    color: Colors.deepPurpleAccent.shade700,
+                  ),
                   title: const Text('Home'),
                   trailing: Text('${state.todoList.length}'),
                   onTap: () => Navigator.pushNamed(context, HomePage.id),
                 );
-              }
+              },
             ),
             Divider(color: Colors.deepPurpleAccent.shade700),
             ListTile(
-              leading: Icon(Icons.delete, color: Colors.deepPurpleAccent.shade700,),
+              leading: Icon(
+                Icons.delete,
+                color: Colors.deepPurpleAccent.shade700,
+              ),
               title: const Text('Bin'),
               trailing: Text('0'),
               onTap: () => Navigator.pushNamed(context, RecycleBin.id),
