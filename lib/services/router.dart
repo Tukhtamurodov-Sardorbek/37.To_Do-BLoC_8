fdc9e02cbc644f0995ecdaff6e3bbbd7b44435c7
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 12:53:55 +0500

    🛡 Code Optimized 🛡

--- lib/services/router.dart
+++ lib/services/router.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/pages/home_page.dart';
+import 'package:todo_app/pages/recycle_bin.dart';
 
-class AppRouter{
-  Route? onGenerate(RouteSettings routeSettings){
-
+class AppRouter {
+  Route? onGenerate(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case HomePage.id:
+        return MaterialPageRoute(builder: (context) => const HomePage());
+      case RecycleBin.id:
+        return MaterialPageRoute(builder: (context) => const RecycleBin());
+    }
+    return null;
   }
 }
