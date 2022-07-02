3399536584ed7e53d3b43384f2d821e0b459c997
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 16:48:39 +0500

    🛡 Code Optimized 🛡

--- lib/services/color_service.dart
+++ lib/services/color_service.dart
@@ -3,5 +3,13 @@ import 'package:flutter/material.dart';
 class ColorService{
   static Color main = Colors.deepPurpleAccent.shade700;
   static const white = Colors.white;
-  static Color grey = Colors.grey.shade300;
+  static Color lightGrey = Colors.grey.shade300;
+
+  // * For theme
+  static const grey = Colors.grey;
+  static const Color black = Colors.black;
+  static const Color lightBlack = Colors.black54;
+  static const Color backgroundDarkTheme = Color(0xff212121);
+  static const Color backgroundLightTheme = Color(0xffe5e5e5);
+
 }
