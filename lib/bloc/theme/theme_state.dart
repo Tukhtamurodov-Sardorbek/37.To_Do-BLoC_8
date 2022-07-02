92e6734ac7c7e11505b6ee044ae847b82ed1d355
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 16:20:27 +0500

    🛡 Code Optimized 🛡

--- lib/bloc/theme/theme_state.dart
+++ lib/bloc/theme/theme_state.dart
@@ -6,19 +6,6 @@ class ThemeState extends Equatable {
 
   @override
   List<Object> get props => [nightMode];
-
-  // * For Hydrated Storage
-  // Map<String, dynamic> toMap() => {
-  //   'todoList': todoList.map((e) => e.toMap()).toList(),
-  //   'deletedList': deletedList.map((e) => e.toMap()).toList(),
-  // };
-  //
-  // factory ToDoState.fromMap(Map<String, dynamic> map) => ToDoState(
-  //   todoList: List<ToDo>.from(map['todoList']?.map((e) => ToDo.fromMap(e))),
-  //   deletedList: List<ToDo>.from(
-  //     map['deletedList']?.map((e) => ToDo.fromMap(e)),
-  //   ),
-  // );
 }
 
 class ThemeInitial extends ThemeState{
