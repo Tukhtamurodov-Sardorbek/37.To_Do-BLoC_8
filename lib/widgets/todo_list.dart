6b4224d1fd351412ee0b18f636fa0515c3155dfe
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 12:30:09 +0500

    🛡 Code Optimized 🛡

--- lib/widgets/todo_list.dart
+++ lib/widgets/todo_list.dart
@@ -13,23 +13,41 @@ class ToDoList extends StatelessWidget {
       padding: const EdgeInsets.only(bottom: 70.0),
       itemCount: list.length,
       itemBuilder: (BuildContext context, int index) {
+        final todo = list[index];
         return GestureDetector(
-          onLongPress: () => context.read<ToDoBloc>().add(DeleteToDo(todo: list[index])),
-          child: CheckboxListTile(
-            title: Text(list[index].title),
-            secondary: Icon(
-              Icons.star_border_outlined,
-              color: Colors.deepPurpleAccent.shade700,
-            ),
-            controlAffinity: ListTileControlAffinity.trailing,
-            activeColor: Colors.deepPurpleAccent.shade700,
-            checkColor: Colors.white,
-            value: list[index].isDone,
-            onChanged: (value) {
-              context.read<ToDoBloc>().add(UpdateToDo(todo: list[index]));
-            },
-          ),
+          onLongPress: () => context.read<ToDoBloc>().add(DeleteToDo(todo: todo)),
+          child: BuildList(todo: todo),
         );
+      },
+    );
+  }
+}
+
+class BuildList extends StatelessWidget {
+  final ToDo todo;
+  const BuildList({Key? key, required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      title: Text(
+        todo.title,
+        style: TextStyle(
+          decoration: todo.isDone ? TextDecoration.lineThrough : TextDecoration.none,
+          decorationColor: Colors.deepPurpleAccent.shade700,
+          decorationThickness: 3,
+        ),
+      ),
+      secondary: Icon(
+        Icons.star_border_outlined,
+        color: Colors.deepPurpleAccent.shade700,
+      ),
+      controlAffinity: ListTileControlAffinity.trailing,
+      activeColor: Colors.deepPurpleAccent.shade700,
+      checkColor: Colors.white,
+      value: todo.isDone,
+      onChanged: (value) {
+        context.read<ToDoBloc>().add(UpdateToDo(todo: todo));
       },
     );
   }
