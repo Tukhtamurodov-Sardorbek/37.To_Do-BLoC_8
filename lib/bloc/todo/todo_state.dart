1a22a0dcdb67b70523c47e98c7b925644e1ea409
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 13:13:15 +0500

    🛡 Code Optimized 🛡

--- lib/bloc/todo/todo_state.dart
+++ lib/bloc/todo/todo_state.dart
@@ -2,20 +2,24 @@ part of 'todo_bloc.dart';
 
 class ToDoState extends Equatable {
   final List<ToDo> todoList;
+  final List<ToDo> deletedList;
   const ToDoState({
     this.todoList = const <ToDo>[],
+    this.deletedList = const<ToDo>[],
   });
 
   @override
-  List<Object> get props => [todoList];
+  List<Object> get props => [todoList, deletedList];
 
   // * For Hydrated Storage
   Map<String, dynamic> toMap() => {
     'todoList' : todoList.map((e) => e.toMap()).toList(),
+    'deletedList' : deletedList.map((e) => e.toMap()).toList(),
   };
 
   factory ToDoState.fromMap(Map<String, dynamic> map) => ToDoState(
     todoList: List<ToDo>.from(map['todoList']?.map((e) => ToDo.fromMap(e))),
+    deletedList: List<ToDo>.from(map['deletedList']?.map((e) => ToDo.fromMap(e))),
   );
 }
 
