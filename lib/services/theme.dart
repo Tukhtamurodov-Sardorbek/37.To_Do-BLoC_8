2a36552e5d7fac0202c2dc91f6538046ec0c66a9
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 17:47:48 +0500

    🛡 Code Optimized 🛡

--- lib/services/theme.dart
+++ lib/services/theme.dart
@@ -21,7 +21,7 @@ class Themes {
       ),
 
       floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: ColorService.lightMain,
+        backgroundColor: ColorService.main,
         foregroundColor: ColorService.white,
       ),
 
