67b7bfeddd3fde66c99ccad0d42d9d5319856125
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 23:32:23 +0500

    🛡 Code Optimized 🛡

--- lib/widgets/bottomSheet.dart
+++ lib/widgets/bottomSheet.dart
@@ -11,108 +11,146 @@ class BottomSheetContent extends StatelessWidget {
   Widget build(BuildContext context) {
     final titleController = TextEditingController();
     final descriptionController = TextEditingController();
-    return SingleChildScrollView(
-      padding: EdgeInsets.fromLTRB(
-        20.0,
-        20.0,
-        20.0,
-        MediaQuery.of(context).viewInsets.bottom,
-      ),
-      child: Column(
-        children: [
-          Text(
-            'Add ToDo',
-            style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                // color: ColorService.main,
-            ),
-            textAlign: TextAlign.center,
+    return BlocBuilder<ThemeBloc, ThemeState>(
+      builder: (context, state) {
+        return SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(
+            20.0,
+            20.0,
+            20.0,
+            MediaQuery.of(context).viewInsets.bottom,
           ),
-          const SizedBox(height: 10.0),
-          TextField(
-            autofocus: true,
-            textInputAction: TextInputAction.next,
-            controller: titleController,
-            decoration: InputDecoration(
-              label: Text(
-                'Title',
-                // style: TextStyle(color: ColorService.main),
-              ),
-              // focusColor: ColorService.main,
-              border: const OutlineInputBorder(),
-              enabledBorder: OutlineInputBorder(
-                // borderSide: BorderSide(color: ColorService.main, width: 5.0),
-              ),
-              focusedBorder: OutlineInputBorder(
-                // borderSide: BorderSide(color: ColorService.main, width: 3.0),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20.0),
-          TextField(
-            maxLines: null,
-            textInputAction: TextInputAction.newline,
-            keyboardType: TextInputType.multiline,
-            controller: descriptionController,
-            decoration: InputDecoration(
-              label: Text(
-                'Description',
-                // style: TextStyle(color: ColorService.main),
-              ),
-              // focusColor: ColorService.main,
-              border: const OutlineInputBorder(),
-              enabledBorder: OutlineInputBorder(
-                // borderSide: BorderSide(color: ColorService.main, width: 5.0),
-              ),
-              focusedBorder: OutlineInputBorder(
-                // borderSide: BorderSide(color: ColorService.main, width: 3.0),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  'Cancel',
-                  style: TextStyle(
-                    // color: ColorService.main,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+              Text(
+                'Add ToDo',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: state.nightMode
+                      ? ColorService.lightMain2
+                      : ColorService.main,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10.0),
+              TextField(
+                autofocus: true,
+                textInputAction: TextInputAction.next,
+                controller: titleController,
+                decoration: InputDecoration(
+                  label: Text(
+                    'Title',
+                    style: TextStyle(
+                      color: state.nightMode
+                          ? ColorService.lightMain2
+                          : ColorService.main,
+                    ),
+                  ),
+                  focusColor: state.nightMode
+                      ? ColorService.lightMain2
+                      : ColorService.main,
+                  border: const OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: state.nightMode
+                            ? ColorService.lightMain2
+                            : ColorService.main,
+                        width: 3.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: state.nightMode
+                            ? ColorService.lightMain2
+                            : ColorService.main,
+                        width: 4.0),
                   ),
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  var todo = ToDo(
-                    id: GUIDGen.generate(),
-                    title: titleController.text.trim(),
-                    description: descriptionController.text.trim(),
-                  );
-                  context.read<ToDoBloc>().add(AddToDo(todo: todo));
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  // primary: ColorService.main,
-                ),
-                child: const Text(
-                  'Add',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
-                    color: ColorService.white,
+              const SizedBox(height: 20.0),
+              TextField(
+                maxLines: null,
+                textInputAction: TextInputAction.newline,
+                keyboardType: TextInputType.multiline,
+                controller: descriptionController,
+                decoration: InputDecoration(
+                  label: Text(
+                    'Description',
+                    style: TextStyle(
+                      color: state.nightMode
+                          ? ColorService.lightMain2
+                          : ColorService.main,
+                    ),
+                  ),
+                  focusColor: state.nightMode
+                      ? ColorService.lightMain2
+                      : ColorService.main,
+                  border: const OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: state.nightMode
+                            ? ColorService.lightMain2
+                            : ColorService.main,
+                        width: 3.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: state.nightMode
+                            ? ColorService.lightMain2
+                            : ColorService.main,
+                        width: 4.0),
                   ),
                 ),
               ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(
+                        color: state.nightMode
+                            ? ColorService.lightMain2
+                            : ColorService.main,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      var todo = ToDo(
+                        id: GUIDGen.generate(),
+                        title: titleController.text.trim(),
+                        description: descriptionController.text.trim(),
+                      );
+                      context.read<ToDoBloc>().add(AddToDo(todo: todo));
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: state.nightMode
+                          ? ColorService.lightMain2
+                          : ColorService.main,
+                    ),
+                    child: const Text(
+                      'Add',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: ColorService.white,
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      ),
+          ),
+        );
+      },
     );
   }
 }
