572a639d78f7666ec2004c7db7706946eacf146a
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 22:58:30 +0500

    🛡 Code Optimized 🛡

--- lib/services/color_service.dart
+++ lib/services/color_service.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class ColorService {
   static Color main = Colors.deepPurpleAccent.shade700;
+  static Color lightMain1 = Colors.deepPurpleAccent.shade400;
+  static Color lightMain2 = Colors.deepPurpleAccent.shade200;
   static const white = Colors.white;
   static Color lightGrey1 = Colors.grey.shade300;
 
