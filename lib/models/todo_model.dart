5cbde66bd7711b5c00ba6c7f93e558ff33784e01
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 22:24:04 +0500

    🛡 Code Optimized 🛡

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -3,21 +3,36 @@ import 'package:equatable/equatable.dart';
 class ToDo extends Equatable {
   final String id;
   final String title;
+  final String description;
   bool isDone;
   bool isDeleted;
 
-  ToDo({required this.id, required this.title, this.isDeleted = false, this.isDone = false});
+  ToDo(
+      {required this.id,
+      required this.title,
+      required this.description,
+      this.isDeleted = false,
+      this.isDone = false});
 
-  ToDo copyWith({String? id, String? title, bool? isDone, bool? isDeleted}) => ToDo(
-    id: id ?? this.id,
-    title: title ?? this.title,
-    isDone: isDone ?? this.isDone,
-    isDeleted: isDeleted ?? this.isDeleted,
-  );
+  ToDo copyWith({
+    String? id,
+    String? title,
+    String? description,
+    bool? isDone,
+    bool? isDeleted,
+  }) =>
+      ToDo(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        isDone: isDone ?? this.isDone,
+        isDeleted: isDeleted ?? this.isDeleted,
+      );
 
   Map<String, dynamic> toMap() => {
         'id': id,
         'title': title,
+        'description': description,
         'isDone': isDone,
         'isDeleted': isDeleted,
       };
@@ -25,10 +40,11 @@ class ToDo extends Equatable {
   factory ToDo.fromMap(Map<String, dynamic> map) => ToDo(
         id: map['id'] ?? '',
         title: map['title'] ?? '',
+        description: map['description'] ?? '',
         isDone: map['isDone'],
         isDeleted: map['isDeleted'],
       );
 
   @override
-  List<Object?> get props => [id, title, isDone, isDeleted];
+  List<Object?> get props => [id, title, description, isDone, isDeleted];
 }
