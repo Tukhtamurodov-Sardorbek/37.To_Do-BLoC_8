8f9e644017b635c8b7a6f061a122d778c6bff97e
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 16:48:43 +0500

    🛡 Code Optimized 🛡

---
+++ lib/services/theme.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/services/color_service.dart';
+
+enum AppTheme {
+  darkTheme,
+  lightTheme,
+}
+
+class Themes {
+  static final AppThemeData = {
+    AppTheme.darkTheme: ThemeData(
+      brightness: Brightness.dark,
+      primarySwatch: ColorService.grey,
+      primaryColor: ColorService.black,
+      backgroundColor: ColorService.backgroundDarkTheme,
+      dividerColor: ColorService.lightBlack,
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: ColorService.white,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(ColorService.white),
+        ),
+      ),
+      textTheme: const TextTheme(
+        subtitle1: TextStyle(color: ColorService.white),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: ColorService.grey,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: ColorService.white,
+      ),
+    ),
+    AppTheme.lightTheme: ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: ColorService.grey,
+      primaryColor: ColorService.white,
+      backgroundColor: ColorService.backgroundLightTheme,
+      dividerColor: Color(0xff757575),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: ColorService.black,
+        foregroundColor: ColorService.white,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(ColorService.black),
+        ),
+      ),
+      textTheme: const TextTheme(
+        subtitle1: TextStyle(color: ColorService.black),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: ColorService.grey,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: ColorService.white,
+      ),
+    ),
+  };
+}
