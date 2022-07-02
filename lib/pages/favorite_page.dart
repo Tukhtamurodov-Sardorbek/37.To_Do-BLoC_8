8b457dd4dcc446a62db14741f55e8a08fe606c56
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 20:11:35 +0500

    🛡 Code Optimized 🛡

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -11,22 +11,7 @@ class FavoritePage extends StatelessWidget {
     return BlocBuilder<ToDoBloc, ToDoState>(
       builder: (context, state) {
         final todoList = state.favoriteList;
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Chip(
-              label: Text(
-                '${todoList.length} ToDos',
-                style: const TextStyle(
-                  color: ColorService.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              backgroundColor: ColorService.main,
-            ),
-            Expanded(child: ToDoList(list: todoList)),
-          ],
-        );
+        return ToDoList(list: todoList);
       },
     );
   }
