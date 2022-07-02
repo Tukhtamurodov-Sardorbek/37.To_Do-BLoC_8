5b6a13d2c48ddb72ae71dd551e1987e06e996119
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 11:52:41 +0500

    🛡 Code Optimized 🛡

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,30 +1,34 @@
 import 'package:equatable/equatable.dart';
 
 class ToDo extends Equatable {
+  final String id;
   final String title;
   bool isDone;
   bool isDeleted;
 
-  ToDo({required this.title, this.isDeleted = false, this.isDone = false});
+  ToDo({required this.id, required this.title, this.isDeleted = false, this.isDone = false});
 
-  ToDo copyWith({String? title, bool? isDone, bool? isDeleted}) => ToDo(
+  ToDo copyWith({String? id, String? title, bool? isDone, bool? isDeleted}) => ToDo(
+    id: id ?? this.id,
     title: title ?? this.title,
     isDone: isDone ?? this.isDone,
     isDeleted: isDeleted ?? this.isDeleted,
   );
 
   Map<String, dynamic> toMap() => {
+        'id': id,
         'title': title,
         'isDone': isDone,
         'isDeleted': isDeleted,
       };
 
   factory ToDo.fromMap(Map<String, dynamic> map) => ToDo(
+        id: map['id'] ?? '',
         title: map['title'] ?? '',
         isDone: map['isDone'],
         isDeleted: map['isDeleted'],
       );
 
   @override
-  List<Object?> get props => [title, isDone, isDeleted];
+  List<Object?> get props => [id, title, isDone, isDeleted];
 }
