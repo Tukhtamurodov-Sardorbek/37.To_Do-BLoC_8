ee96aeed16d1e236594e523fd02381fc9e4e2ac7
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-02 18:31:35 +0500

    🛡 Code Optimized 🛡

---
+++ lib/pages/favorite_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/bloc/bloc_exports.dart';
+import 'package:todo_app/services/color_service.dart';
+import 'package:todo_app/widgets/todo_list.dart';
+
+class FavoritePage extends StatelessWidget {
+  const FavoritePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ToDoBloc, ToDoState>(
+      builder: (context, state) {
+        final todoList = state.todoList;
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Chip(
+              label: Text(
+                '${state.todoList.length} ToDos',
+                style: const TextStyle(
+                  color: ColorService.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              backgroundColor: ColorService.main,
+            ),
+            Expanded(child: ToDoList(list: todoList)),
+          ],
+        );
+      },
+    );
+  }
+}
