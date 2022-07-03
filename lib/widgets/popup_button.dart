598ff6ce518f7dbf272e47fa0984321938ae1cdf
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-03 12:51:00 +0500

    🛡 Code Optimized 🛡

--- lib/widgets/popup_button.dart
+++ lib/widgets/popup_button.dart
@@ -1,66 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/bloc/bloc_exports.dart';
+import 'package:todo_app/models/todo_model.dart';
 import 'package:todo_app/services/color_service.dart';
 
 class PopUpButton extends StatelessWidget {
-  final bool isDeleted;
-  final VoidCallback deleteFunc;
+  final ToDo todo;
+  final VoidCallback mark;
+
   const PopUpButton({
     Key? key,
-    required this.deleteFunc,
-    required this.isDeleted,
+    required this.todo,
+    required this.mark,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton(
-      splashRadius: 1,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      itemBuilder: isDeleted
-          ? ((context) {
-              return [
-                PopupMenuItem(
-                  child: ListTile(
-                    title: Text('Restore', style: TextStyle(color: ColorService.main, fontWeight: FontWeight.bold),),
-                    leading: Icon(Icons.restore_from_trash, color: ColorService.green,),
-                  ),
-                  onTap: () {},
-                ),
-                PopupMenuItem(
-                  onTap: deleteFunc,
-                  child: ListTile(
-                    title: Text('Delete Permanently', style: TextStyle(color: ColorService.main, fontWeight: FontWeight.bold),),
-                    leading: Icon(Icons.delete_forever, color: ColorService.red,),
-                  ),
-                ),
-              ];
-            })
-          : ((context) {
-              return [
-                PopupMenuItem(
-                  child: ListTile(
-                    title: Text('Edit', style: TextStyle(color: ColorService.main, fontWeight: FontWeight.bold),),
-                    leading: Icon(Icons.edit, color: ColorService.main),
-                  ),
-                  onTap: () {},
-                ),
-                PopupMenuItem(
-                  child: ListTile(
-                    title: Text('Add to bookmark', style: TextStyle(color: ColorService.main, fontWeight: FontWeight.bold),),
-                    leading: Icon(Icons.bookmark, color: ColorService.main),
-                  ),
-                  onTap: () {},
-                ),
-                PopupMenuItem(
-                  onTap: deleteFunc,
-                  child: ListTile(
-                    title: Text('Delete', style: TextStyle(color: ColorService.main, fontWeight: FontWeight.bold),),
-                    leading: Icon(Icons.delete, color: ColorService.main),
-                  ),
-                ),
-              ];
-            }),
+    return BlocBuilder<ThemeBloc, ThemeState>(
+      builder: (context, state) {
+        return PopupMenuButton(
+          splashRadius: 1,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          itemBuilder: todo.isDeleted
+              ? ((context) {
+                  return [
+                    PopupMenuItem(
+                      child: ListTile(
+                        title: Text('Restore', style: TextStyle(color: ColorService.main, fontWeight: FontWeight.bold),),
+                        leading: Icon(Icons.restore_from_trash, color: ColorService.green,),
+                      ),
+                      onTap: () {},
+                    ),
+                    PopupMenuItem(
+                      onTap: (){
+                        return context.read<ToDoBloc>().add(DeleteToDo(todo: todo));
+                      },
+                      child: ListTile(
+                        title: Text('Delete Permanently', style: TextStyle(color: ColorService.main, fontWeight: FontWeight.bold),),
+                        leading: Icon(Icons.delete_forever, color: ColorService.red,),
+                      ),
+                    ),
+                  ];
+                })
+              : ((context) {
+                  return [
+                    PopupMenuItem(
+                      child: ListTile(
+                        title: Text('Edit', style: TextStyle(color: state.nightMode ? ColorService.lightMain2 : ColorService.main, fontWeight: FontWeight.bold),),
+                        leading: Icon(Icons.edit, color: state.nightMode ? ColorService.lightMain2 : ColorService.main),
+                      ),
+                      onTap: () {},
+                    ),
+                    PopupMenuItem(
+                      child: ListTile(
+                        title: Text(todo.isFavorite ? 'Remove from bookmark' :  'Add to bookmark', style: TextStyle(color: state.nightMode ? ColorService.lightMain2 : ColorService.main, fontWeight: FontWeight.bold),),
+                        leading: Icon(todo.isFavorite ? Icons.bookmark_remove : Icons.bookmark_add, color: state.nightMode ? ColorService.lightMain2 : ColorService.main),
+                      ),
+                      onTap: () {
+                        return context.read<ToDoBloc>().add(MarkFavoriteOrUnfavoriteToDO(todo: todo));
+                      },
+                    ),
+                    PopupMenuItem(
+                      onTap: (){
+                        return context.read<ToDoBloc>().add(RemoveToDo(todo: todo));
+                      },
+                      child: ListTile(
+                        title: Text('Delete', style: TextStyle(color: state.nightMode ? ColorService.lightMain2 : ColorService.main, fontWeight: FontWeight.bold),),
+                        leading: Icon(Icons.delete, color: state.nightMode ? ColorService.lightMain2 : ColorService.main),
+                      ),
+                    ),
+                  ];
+                }),
+        );
+      }
     );
   }
 }
