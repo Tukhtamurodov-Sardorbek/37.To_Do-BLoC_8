831a169eb20dc0c6a2b51037b7fef7ceaa21d512
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-07-03 11:00:38 +0500

    🛡 Code Optimized 🛡

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -4,47 +4,61 @@ class ToDo extends Equatable {
   final String id;
   final String title;
   final String description;
-  bool isDone;
-  bool isDeleted;
+  final String createdTime;
+  final bool isDone;
+  final bool isDeleted;
+  final bool isFavorite;
 
-  ToDo(
-      {required this.id,
-      required this.title,
-      required this.description,
-      this.isDeleted = false,
-      this.isDone = false});
+  const ToDo({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.createdTime,
+    this.isDeleted = false,
+    this.isDone = false,
+    this.isFavorite = false,
+  });
 
   ToDo copyWith({
     String? id,
     String? title,
     String? description,
+    String? createdTime,
     bool? isDone,
     bool? isDeleted,
+    bool? isFavorite,
   }) =>
       ToDo(
         id: id ?? this.id,
         title: title ?? this.title,
         description: description ?? this.description,
+        createdTime: createdTime ?? this.createdTime,
         isDone: isDone ?? this.isDone,
         isDeleted: isDeleted ?? this.isDeleted,
+        isFavorite: isFavorite ?? this.isFavorite,
       );
 
   Map<String, dynamic> toMap() => {
         'id': id,
         'title': title,
         'description': description,
+        'createdTime': createdTime,
         'isDone': isDone,
         'isDeleted': isDeleted,
+        'isFavorite': isFavorite,
       };
 
   factory ToDo.fromMap(Map<String, dynamic> map) => ToDo(
         id: map['id'] ?? '',
         title: map['title'] ?? '',
         description: map['description'] ?? '',
+        createdTime: map['createdTime'] ?? '',
         isDone: map['isDone'],
         isDeleted: map['isDeleted'],
+        isFavorite: map['isFavorite'],
       );
 
   @override
-  List<Object?> get props => [id, title, description, isDone, isDeleted];
+  List<Object?> get props =>
+      [id, title, description, createdTime, isDone, isDeleted, isFavorite];
 }
